6a98c271ef9aad526398446a140c7492f01df363
Jim Andreasen <reifnir@gmail.com>
2019-06-02 07:47:07 -0400

    added detail about code that runs on startup but BEFORE any messages enter the system

--- src/Reifnir.Manager.Acquisition.Service/AcquisitionService.cs
+++ src/Reifnir.Manager.Acquisition.Service/AcquisitionService.cs
@@ -32,6 +32,20 @@ namespace Reifnir.Manager.Acquisition.Service
         }
 
         /// <summary>
+        /// Any additional processing that must be run during service initialization would go in here. For
+        /// Example: subscribe to events, loading reference data into memory, setup an ORM session factory
+        /// in a Resource Access component...
+        /// 
+        /// Note from Microsoft: The runtime waits for OpenAsync to finish before marking the replica as ready
+        ///     https://github.com/Azure/service-fabric-issues/issues/1257
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        protected override Task OnOpenAsync(CancellationToken cancellationToken)
+        {
+            return base.OnOpenAsync(cancellationToken);
+        }
+        /// <summary>
         /// This is the main entry point for your service instance.
         /// </summary>
         /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service instance.</param>
